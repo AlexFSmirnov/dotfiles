052315346b0a5d47a183086fc2dcb37c6053ce08
AlexFSmirnov <alex.f.smirnov@yandex.ru>
2017-05-21 19:41:05 +0300

    If there is no such paste format as &filename, None is used.

--- vim/vim-pastebin.py
+++ vim/vim-pastebin.py
@@ -14,21 +14,28 @@ def get_range():
     return "\n".join(lines)
 
 
+def paste_to_pastebin(paste_format):
+    r = requests.post(url, dict(
+        api_option="paste",
+        api_dev_key=user_dev_key,
+        api_paste_code=get_range(),
+        api_paste_private="1",
+        api_paste_format=paste_format))
+    return r.text
+
+
 url = "https://pastebin.com/api/api_post.php"
 try:
     user_dev_key = open("/home/alexfsmirnov/.pastebin_dev_key").read().strip()
 except IOError:
     raise IOError("No pastebin dev key found")
 
-r = requests.post(url, dict(
-    api_option="paste",
-    api_dev_key=user_dev_key,
-    api_paste_code=get_range(),
-    api_paste_private="1",
-    api_paste_format=vim.eval("&filetype")))
+r = paste_to_pastebin(vim.eval("&filetype"))
+if not r.startswith("http"):
+    r = paste_to_pastebin(None)
 
-vim.command("let cp = input('{}. Copy to clipboard? (y/N) ')".format(r.text))
+vim.command("let cp = input('{}. Copy to clipboard? (y/N) ')".format(r))
 cp = vim.eval("cp")
 if cp.lower() == 'y':
-    pyperclip.copy(r.text)
+    pyperclip.copy(r)
     print(" Copy success.")
