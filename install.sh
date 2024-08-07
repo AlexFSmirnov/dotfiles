b5a52ac0bb20a00aaa004c628120add34be282f9
AlexFSmirnov <alex.f.smirnov@gmail.com>
2024-08-07 17:41:36 +0200

    install script fixes

--- install.sh
+++ install.sh
@@ -7,22 +7,22 @@ cp -a zsh/. ~
 cp -a tmux/. ~
 
 cp -a vim/. ~
-mkdir ~/.vim/undodir
-mkdir ~/.vim/backups
-mkdir ~/.vim/swapfiles
+mkdir -p ~/.vim/undodir
+mkdir -p ~/.vim/backups
+mkdir -p ~/.vim/swapfiles
 
 mkdir -p ~/.config/nvim
 cp -a nvim/. ~/.config/nvim
 
 mkdir -p /usr/local/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Bold/HackNerdFont-Bold.ttf -O /usr/local/share/fonts/HackNerdFont-Bold.ttf
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf -O /usr/local/share/fonts/HackNerdFont-Regular.ttf
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Italic/HackNerdFont-Italic.ttf -O /usr/local/share/fonts/HackNerdFont-Italic.ttf
-wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/BoldItalic/HackNerdFont-BoldItalic.ttf -O /usr/local/share/fonts/HackNerdFont-BoldItalic.ttf
+sudo wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Bold/HackNerdFont-Bold.ttf -O /usr/local/share/fonts/HackNerdFont-Bold.ttf
+sudo wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf -O /usr/local/share/fonts/HackNerdFont-Regular.ttf
+sudo wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Italic/HackNerdFont-Italic.ttf -O /usr/local/share/fonts/HackNerdFont-Italic.ttf
+sudo wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/BoldItalic/HackNerdFont-BoldItalic.ttf -O /usr/local/share/fonts/HackNerdFont-BoldItalic.ttf
 
 mkdir -p ~/.config/alacritty
 cp -a alacritty/. ~/.config/alacritty
 
-sudo apt install fd
+sudo apt install fd-find
 sudo apt install ripgrep
 sudo apt install unzip
