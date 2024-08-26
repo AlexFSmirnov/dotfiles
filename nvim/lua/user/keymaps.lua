f809ee2ac9bfd35212938607f3e120cde7f10c09
AlexFSmirnov <alex.f.smirnov@gmail.com>
2024-08-26 14:22:00 +0200

    Updated keybinds

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -32,11 +32,9 @@ map("", "L", "$")
 map("i", "<C-BS>", "<C-w>")
 map("c", "<C-BS>", "<C-w>", { silent = false })
 
--- Clear search highlighting with <leader> and c
-map("n", "<leader>lc", "<cmd>nohl<CR>", { desc = "Clear search highlighting" })
-
--- Clear Windows line breaks with <leader> and m
-map("n", "<leader>m", "<cmd>%s/\\r//g<CR>", { desc = "Clear Windows line breaks" })
+-- Utils
+map("n", "<leader>kc", "<cmd>nohl<CR>", { desc = "Clear search highlighting" })
+map("n", "<leader>km", "<cmd>%s/\\r//g<CR>", { desc = "Clear Windows line breaks" })
 
 -- Buffer navigation
 map("n", "<leader><tab>", "<cmd>b#<cr>", { desc = "Switch to last used buffer" })
